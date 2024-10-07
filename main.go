1398eb1770484808515d44c9ebe5780d418364fd
Santo <hectorgool@gmail.com>
2024-10-07 13:01:35 -0600

    add func processCSVFile

--- main.go
+++ main.go
@@ -1,9 +1,43 @@
 package main
 
 import (
+	"encoding/csv"
 	"fmt"
+	"os"
 )
 
 func main() {
-	fmt.Println("Stori Challenge")
+	//.Println("Stori Challenge")
+	filePath := "txns.csv"
+	err := processCSVFile(filePath)
+	if err != nil {
+		fmt.Println("Error:", err)
+	} else {
+		fmt.Println("El archivo CSV es válido.")
+	}
+}
+
+func processCSVFile(filePath string) error {
+	// Abrir el archivo CSV
+	file, err := os.Open(filePath)
+	if err != nil {
+		return fmt.Errorf("error al abrir el archivo: %v", err)
+	}
+	defer file.Close()
+
+	// Leer el archivo CSV
+	reader := csv.NewReader(file)
+	reader.TrimLeadingSpace = true
+
+	// Leer todas las filas
+	rows, err := reader.ReadAll()
+	if err != nil {
+		return fmt.Errorf("error al leer las filas: %v", err)
+	}
+
+	for _, row := range rows {
+		fmt.Printf("%v, %v, %v\n", row[0], row[1], row[2])
+	}
+
+	return nil
 }
