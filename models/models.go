31e0f2b8d9ffb462b01807fd86eaa703b5cb1e9b
Santo <hectorgool@gmail.com>
2024-10-07 16:16:20 -0600

    add SQLDocument struct

--- models/models.go
+++ models/models.go
@@ -4,4 +4,10 @@ type (
 	CSVDocument struct {
 		Id, Date, Transaction string
 	}
+	SQLDocument struct {
+		Id            uint    `gorm:"primaryKey"`
+		IdTransaction uint    `json:"idTransaction"`
+		Date          string  `gorm:"type:date"`
+		Transaction   float64 `json:"transaction"`
+	}
 )
