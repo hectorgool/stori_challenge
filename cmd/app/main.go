d212015c91c85dde2fc1dc6c1c8cb83026ff78bd
Santo <hectorgool@gmail.com>
2024-10-09 17:43:19 -0600

    update, main with args

--- cmd/app/main.go
+++ cmd/app/main.go
@@ -9,7 +9,20 @@ import (
 )
 
 func main() {
-	const filePath = "txns.csv" // Usar constante para la ruta del archivo
+
+	// Verifica que se hayan pasado exactamente dos argumentos adicionales
+	if len(os.Args) != 3 {
+		log.Fatal("Se requieren dos argumentos: <email> <path al archivo CSV>")
+	}
+
+	// Obtiene los argumentos de la consola
+	emailWithSummary := os.Args[1]
+	filePath := os.Args[2]
+
+	// Valida el formato del correo electrónico
+	if !email.IsValidEmail(emailWithSummary) {
+		log.Fatalf("El formato del correo electrónico: %v es inválido", emailWithSummary)
+	}
 
 	if err := csv.CheckFileSize(filePath); err != nil {
 		log.Fatalf("Error: The file %s exceeds the size limit of %s MB defined by FILE_SIZE_LIMIT. Details: %v", filePath, os.Getenv("FILE_SIZE_LIMIT"), err)
@@ -23,6 +36,7 @@ func main() {
 	if err != nil {
 		log.Fatalf("Failed to create summary: %v", err)
 	}
+	emailData.EmailTo = emailWithSummary
 	log.Printf("Email data generated: %+v \n", emailData) // Loguear emailData
 
 	if err := email.SendEmail(emailData); err != nil {
