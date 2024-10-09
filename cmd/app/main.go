a75f11a3c2627523c28aef18c9e4e277f2a0f279
Santo <hectorgool@gmail.com>
2024-10-09 11:24:00 -0600

    add csv.CheckFileSize validation

--- cmd/app/main.go
+++ cmd/app/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"log"
+	"os"
 	"stori_challenge/pkg/csv"
 	"stori_challenge/pkg/email"
 	"stori_challenge/pkg/summary"
@@ -9,6 +10,10 @@ import (
 
 func main() {
 	const filePath = "txns.csv" // Usar constante para la ruta del archivo
+
+	if err := csv.CheckFileSize(filePath); err != nil {
+		log.Fatalf("Error: The file %s exceeds the size limit of %s MB defined by FILE_SIZE_LIMIT. Details: %v", filePath, os.Getenv("FILE_SIZE_LIMIT"), err)
+	}
 
 	if err := csv.ProcessCSVFile(filePath); err != nil {
 		log.Fatalf("Error processing CSV file (%s): %v", filePath, err)
@@ -21,7 +26,6 @@ func main() {
 	log.Printf("Email data generated: %+v \n", emailData) // Loguear emailData
 
 	if err := email.SendEmail(emailData); err != nil {
-		// Manejo de errores
 		log.Fatalf("Error al enviar el correo: %v\n", err)
 	}
 
