c4d66bc0f2ab34b2ecd93f963efbcbae41dbf192
Santo <hectorgool@gmail.com>
2024-10-10 16:46:18 -0600

    move func handleCSVUpload to handlers dir

--- cmd/app/main.go
+++ cmd/app/main.go
@@ -1,90 +1,34 @@
 package main
 
 import (
-	"net/http"
+	"log"
 	"os"
-	"stori_challenge/pkg/csv"
-	"stori_challenge/pkg/email"
-	"stori_challenge/pkg/summary"
+	"path/filepath"
+	"stori_challenge/internal/handlers"
 
 	"github.com/gin-gonic/gin"
+	"github.com/joho/godotenv"
 )
 
 func main() {
+
+	// Ruta al archivo .env en la raíz del proyecto
+	envPath := filepath.Join(".", ".env")
+
+	// Cargar el archivo .env
+	err := godotenv.Load(envPath)
+	if err != nil {
+		log.Fatalf("Error al cargar archivo: %v", err)
+	}
+
 	r := gin.Default()
 
 	r.GET("/", func(c *gin.Context) {
 		c.JSON(200, gin.H{
-			"message": "Hola, Stori!",
+			"message": "Hola, Stori",
 		})
 	})
 
-	r.POST("/csv", handleCSVUpload)
-
-	port := os.Getenv("PORT")
-	if port == "" {
-		port = "8080"
-	}
-	r.Run(":" + port)
-}
-
-func handleCSVUpload(c *gin.Context) {
-	// Obtener el email del formulario
-	emailWithSummary := c.PostForm("email")
-
-	// Validar el formato del correo electrónico
-	if !email.IsValidEmail(emailWithSummary) {
-		c.JSON(http.StatusBadRequest, gin.H{"error": "Formato de correo electrónico inválido"})
-		return
-	}
-
-	// Obtener el archivo CSV
-	file, err := c.FormFile("file")
-	if err != nil {
-		c.JSON(http.StatusBadRequest, gin.H{"error": "No se pudo obtener el archivo"})
-		return
-	}
-
-	// Crear un archivo temporal para guardar el CSV
-	tempFile, err := os.CreateTemp("", "csv-*.csv")
-	if err != nil {
-		c.JSON(http.StatusInternalServerError, gin.H{"error": "No se pudo crear un archivo temporal"})
-		return
-	}
-	defer os.Remove(tempFile.Name())
-	defer tempFile.Close()
-
-	// Guardar el archivo subido en el archivo temporal
-	if err := c.SaveUploadedFile(file, tempFile.Name()); err != nil {
-		c.JSON(http.StatusInternalServerError, gin.H{"error": "No se pudo guardar el archivo"})
-		return
-	}
-
-	// Verificar el tamaño del archivo
-	if err := csv.CheckFileSize(tempFile.Name()); err != nil {
-		c.JSON(http.StatusBadRequest, gin.H{"error": "El archivo excede el límite de tamaño permitido"})
-		return
-	}
-
-	// Procesar el archivo CSV
-	if err := csv.ProcessCSVFile(tempFile.Name()); err != nil {
-		c.JSON(http.StatusInternalServerError, gin.H{"error": "Error al procesar el archivo CSV"})
-		return
-	}
-
-	// Crear el resumen
-	emailData, err := summary.CreateSummary()
-	if err != nil {
-		c.JSON(http.StatusInternalServerError, gin.H{"error": "Error al crear el resumen"})
-		return
-	}
-	emailData.EmailTo = emailWithSummary
-
-	// Enviar el email
-	if err := email.SendEmail(emailData); err != nil {
-		c.JSON(http.StatusInternalServerError, gin.H{"error": "Error al enviar el correo"})
-		return
-	}
-
-	c.JSON(http.StatusOK, gin.H{"message": "Archivo CSV procesado y resumen enviado exitosamente"})
+	r.POST("/csv", handlers.HandleCSVUpload)
+	r.Run(":" + os.Getenv("HOST_PORT"))
 }
